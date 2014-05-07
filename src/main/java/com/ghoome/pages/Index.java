d741fd545488fd9978010fbc482aa99660ac7a85
liwei <hovey727@126.com>
2014-05-07 20:35:15 +0800

    修正数据获取

--- src/main/java/com/ghoome/pages/Index.java
+++ src/main/java/com/ghoome/pages/Index.java
@@ -1,28 +1,59 @@
 package com.ghoome.pages;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 
+import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Zone;
 import org.springframework.data.authentication.UserCredentials;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
+import com.ghoome.fl.domain.Bjdc500Record;
 import com.mongodb.MongoClient;
 
 public class Index {
+	private static final SimpleDateFormat SSDF = new SimpleDateFormat("yyyyMMdd");
 	
 	@Persist MongoTemplate template;
-	@Property Set<String> dbNames;
-	@Property String dbName;
+	@Property List<Bjdc500Record> records;
+	@Property Bjdc500Record m;
+	@Property @Persist String input;
+	
+	@InjectComponent Zone gzone;
 	
 	void setupRender() throws IOException{
 		if(template == null){
 			template = configMongoTemplate();
 		}
 		
-		dbNames = template.getCollectionNames();
+		if(input == null){
+			input = SSDF.format(new Date());
+		}
+		
+		refreshRonaldo();
+	}
+	
+	Object onActionFromForm(){
+		refreshRonaldo();
+		return gzone.getBody();
+	}
+	
+	private void refreshRonaldo(){
+		records = template.find(
+			new Query(
+				Criteria.where("monitorNo").is(Integer.valueOf(input))
+			).with(new Sort(Sort.Direction.ASC, "no")).limit(200), 
+			Bjdc500Record.class
+		);
 	}
 	
 	private MongoTemplate configMongoTemplate() throws IOException{
@@ -39,5 +70,14 @@ public class Index {
 			new UserCredentials(property.getProperty("username"),
 					property.getProperty("password"))
 		);
+	}
+	
+	public double scaleDouble(double raw, int width){
+
+		if(!Double.isInfinite(raw) && !Double.isNaN(raw)){
+			return new BigDecimal(raw).setScale(width, BigDecimal.ROUND_HALF_UP).doubleValue();
+		}
+		
+		return -99;
 	}
 }
